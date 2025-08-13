85ac555761a3ee9cbe4de1a683269df00b33c451
lyx memo <lyxmemo@gmail.com>
2025-08-13 05:52:34 +0000

    add more photos

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/.nojekyll");
   eleventyConfig.addWatchTarget("src/css/");
 
   // Add photos collection
